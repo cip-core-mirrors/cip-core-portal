6995ef754d8a0ed44c21481e60ee2721f579b03e
Mathieu <mathieu.0598@gmail.com>
2020-04-24 18:58:02 +0200

    Update gatekeeper and site conf

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /opt/conf 
+
+
+sed -e "s#\${clientId}#${OPENID_CLIENT_ID}#" -e \
+        "s#\${clientSecret}#${OPENID_CLIENT_SECRET}#" -e \
+        "s#\${realmUrl}#${OPENID_REALM_URL}#" -e \
+        "s#\${encryptionKey}#${ENCRYPTION_KEY}#" -e \
+        "s#\${upstreamUrl}#${GATEKEEPER_UPSTREAM_URL}#" -e \
+        "s#\${redirectionUrl}#${GATEKEEPER_REDIRECTION_URL}#" \
+        gatekeeper-conf.yaml > conf.yaml
+
+
+exec "$@"
