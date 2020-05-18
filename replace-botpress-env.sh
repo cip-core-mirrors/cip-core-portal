8f15985b22a020be55f57a55e86035f66e336097
Mathieu <mathieu.0598@gmail.com>
2020-05-18 19:32:07 +0200

    Add script for replace botpress value

---
+++ replace-botpress-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$BOTPRESS_URL" ] || [ -z "$BOTPRESS_NAME" ]
+then
+      echo "Cannot update botpress dependencies please set BOTPRESS_URL  and BOTPRESS_NAME"
+      exit;
+fi;
+
+while read -r line ; do
+    echo "Processing $line"
+
+    sed -ri 's/\{\{--BOTPRESS_URL--\}\}/'$BOTPRESS_URL'/' $line
+    sed -ri 's/\{\{--BOTPRESS_NAME--\}\}/'$BOTPRESS_NAME'/' $line
+
+    # your code goes here
+done < <(grep -rl '{{--\w*--}}' $1)
